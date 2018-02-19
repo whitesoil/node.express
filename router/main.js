3e16c7d3e0a08e41970279737d73f4c5d10a6664
whitesoil <whitesoil.kr@gmail.com>
2018-02-20 01:45:40 +0900

    Session

--- router/main.js
+++ router/main.js
@@ -1,9 +1,12 @@
 module.exports = function(app, fs) {
-  app.get('/', function(req, res) {
-    res.render('index', {
-      title: "My Homepage",
-      length: 5
-    });
+  app.get('/',function(req,res){
+      var sess = req.session;
+      res.render('index', {
+          title: "MY HOMEPAGE",
+          length: 5,
+          name: sess.name,
+          username: sess.username
+      });
   });
 
   app.get('/list', function(req, res) {
@@ -86,8 +89,8 @@ module.exports = function(app, fs) {
             "success": 1
           };
           res.json(result);
-        })
-    })
+        });
+    });
   });
 
   app.delete('/deleteUser/:username', function(req, res) {
@@ -114,8 +117,54 @@ module.exports = function(app, fs) {
           result["success"] = 1;
           res.json(result);
           return;
-        })
-    })
+        });
+    });
 
-  })
+  });
+
+  app.get('/login/:username/:password', function(req, res){
+         var sess;
+         sess = req.session;
+
+         fs.readFile(__dirname + "/../data/user.json", "utf8", function(err, data){
+             var users = JSON.parse(data);
+             var username = req.params.username;
+             var password = req.params.password;
+             var result = {};
+             if(!users[username]){
+                 // USERNAME NOT FOUND
+                 result["success"] = 0;
+                 result["error"] = "not found";
+                 res.json(result);
+                 return;
+             }
+
+             if(users[username]["password"] == password){
+                 result["success"] = 1;
+                 sess.username = username;
+                 sess.name = users[username]["name"];
+                 res.json(result);
+
+             }else{
+                 result["success"] = 0;
+                 result["error"] = "incorrect";
+                 res.json(result);
+             }
+         });
+     });
+
+     app.get('/logout', function(req, res){
+         sess = req.session;
+         if(sess.username){
+             req.session.destroy(function(err){
+                 if(err){
+                     console.log(err);
+                 }else{
+                     res.redirect('/');
+                 }
+             })
+         }else{
+             res.redirect('/');
+         }
+     });
 }
